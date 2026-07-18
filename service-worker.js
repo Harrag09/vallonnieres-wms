deb074e0caa1a70269792a730907071254046d46
Harrag09 <harragoussama@gmail.com>
2026-07-18 13:33:20 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,16 @@
-const CACHE_NAME = 'vallonieres-cache-v1';
-const urlsToCache = [
-  '/vallonnieres-wms/',
-  '/vallonnieres-wms/index.html',
-  '/vallonnieres-wms/manifest.json'
-];
-
-// Install: Cache static assets
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+  self.skipWaiting();
 });
 
-// Fetch: Serve from cache, then network
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
+});
 });
