22f4ec46a12fa29baa9aa202f895d6633ef71acb
Harrag09 <harragoussama@gmail.com>
2026-07-18 12:56:40 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'vallonieres-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  '/vallonnieres-wms/',
+  '/vallonnieres-wms/index.html',
+  '/vallonnieres-wms/manifest.json'
 ];
 
 // Install: Cache static assets
